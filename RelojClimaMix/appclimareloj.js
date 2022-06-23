e59ac481dcfa3bfaf34d9806f3f77c63197a1a99
NuPaCoding <105621510+NuPaCoding@users.noreply.github.com>
2022-06-22 22:46:41 -0300

    Update appclimareloj.js

--- RelojClimaMix/appclimareloj.js
+++ RelojClimaMix/appclimareloj.js
@@ -28,7 +28,7 @@
         var meses = ['01','02','03','04','05','06','07','08','09','10','11','12'];
         pMes.textContent = meses[mes]
 
-        pYear.textContent = year;
+        pYear.textContent = year - 2000;
 
         if (horas >= 12) {
             horas = horas - 12;
